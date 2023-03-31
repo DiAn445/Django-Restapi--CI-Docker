1007e2fb407fb712e37255c1e98960fb092ed1e5
Dmytro <103268527+DiAn445@users.noreply.github.com>
2023-03-31 19:16:07 +0300

    GitHub Actions

--- Django_cats/Cat_kinds/tests.py
+++ Django_cats/Cat_kinds/tests.py
@@ -1,9 +1,21 @@
 from django.test import TestCase
-
-from Django_cats.Cat_kinds.models import CatKinds
+from django.conf import settings
+from .utils import CatKinds
 
 class UnitTest(TestCase):
-    def amount_of_obj_in_db(self):
+    fixtures = ['myfixture.json']
+    
+    def test_amount_of_obj_in_db(self):
+        db = CatKinds.objects.count()
+        self.assertEqual(db, 6)
+
+    def test_likes_are_positive(self):
         db = CatKinds.objects.all()
-        return self.assertEqual(len(db), 6)
-        
+        result = [i.likes for i in db]
+        match result:
+            case result if min(result) < 0:
+                raise ValueError("Error: the result should be >= 0")
+        self.assertEqual(True, True)    
+
+
+

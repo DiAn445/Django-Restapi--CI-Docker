685d0e485923fe90f47a41719c75e2abc8dbb8e9
Dmytro <103268527+DiAn445@users.noreply.github.com>
2023-03-31 17:10:24 +0300

    tests

--- Django_cats/Cat_kinds/tests.py
+++ Django_cats/Cat_kinds/tests.py
@@ -1,4 +1,9 @@
 from django.test import TestCase
 
+from Django_cats.Cat_kinds.models import CatKinds
 
-
+class UnitTest(TestCase):
+    def amount_of_obj_in_db(self):
+        db = CatKinds.objects.all()
+        return self.assertEqual(len(db), 6)
+        
--- Django_cats/Cat_kinds/urls.py
+++ Django_cats/Cat_kinds/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from Cat_kinds.views import *
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', index, name='home'),
@@ -7,5 +8,6 @@ urlpatterns = [
     path('sign_in/', LogInUser.as_view(), name='sign_in'),
     path('log_out/', logout_user, name='log_out'),
     path('Anya/', about, name='Anya'),
-    path('post/<int:post_id>/', show_post, name='post')
+    path('post/<int:post_id>/', show_post, name='post'),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico', permanent=True))
 ]
